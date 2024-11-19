f6c9acb48ac9c3ae0b1406621fbb7a7853f8908a
VusalIsayeV <vvusalisayevv@gmail.com>
2024-11-19 12:28:40 +0400

    commit 2

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
-
+  
 Route::resource('FullTask', TaskController::class);
 Route::patch('FullTask/done/{id}', [TaskController::class, 'done']);
 Route::patch('FullTask/delete/{id}', [TaskController::class, 'DeleteTask']);
