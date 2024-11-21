31c84f9501c9ec321c1afaae00193293bf46dd75
VusalIsayeV <vvusalisayevv@gmail.com>
2024-11-21 09:28:18 +0400

    Feature Test

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
   
 Route::resource('FullTask', TaskController::class);
 Route::patch('FullTask/done/{id}', [TaskController::class, 'done']);
-Route::patch('FullTask/delete/{id}', [TaskController::class, 'DeleteTask']);
-// Route::get('FullTask/', [TaskController::class, 'DeleteTask']);
+Route::delete('FullTask/delete/{id}', [TaskController::class, 'DeleteTask']);
+
 
